1fd586342a8183f84f3e8b5f413660e9a2b8a278
stive <chen_hai888@126.com>
2021-02-15 17:40:23 +0800

    整合nacos配置中心,并测试

--- guli-coupon/src/main/java/com/bangdao/guli/coupon/controller/CouponController.java
+++ guli-coupon/src/main/java/com/bangdao/guli/coupon/controller/CouponController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +27,23 @@ import com.bangdao.common.utils.R;
  * @email chen_hai888@126.com
  * @date 2021-02-15 10:41:58
  */
+@RefreshScope   //nacos配置刷新注解
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",name).put("age",age);
+    }
+
 
     @RequestMapping("/member/list")
     public R membercoupons(){
